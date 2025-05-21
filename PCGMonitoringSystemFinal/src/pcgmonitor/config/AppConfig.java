9abf7ac75d54f6c31fba9ffa5924959e5dad8661
duembi <161377352+duembi@users.noreply.github.com>
2025-05-21 11:04:51 +0300

    Delete PCGMonitoringSystemFinal directory

--- PCGMonitoringSystemFinal/src/pcgmonitor/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package pcgmonitor.config;
-
-public final class AppConfig {
-    private AppConfig() {}
-    public static final double DEFAULT_SAMPLING_RATE = 1000.0;
-    public static final int DEFAULT_MOVING_AVERAGE_WINDOW = 5;
-    public static final double DEFAULT_PEAK_THRESHOLD = 0.3;
-}
